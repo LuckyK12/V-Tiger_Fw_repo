8d7e8f4de03dbbbc6c11fcaa8ccaf1466dcc347d
NEW <NEW@DESKTOP-5MUK57H>
2025-01-28 16:53:14 +0530

    Added class of practice

---
+++ Datadriventesting/src/test/java/testng/Newpullreq.java
@@ -0,0 +1,12 @@
+package testng;
+
+import org.testng.annotations.Test;
+
+public class Newpullreq {
+	@Test
+	public void practPullr() {
+		System.out.println("kl");
+		System.out.println("lk");
+	}
+
+}
